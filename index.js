1fe2ee1e49eaa08d8b22aa7c7a7fa94fc7444793
matthew <larochelle.matthew@gmail.com>
2023-06-27 14:17:15 -0700

    edit prompt for chatbot

--- index.js
+++ index.js
@@ -22,18 +22,34 @@ client.on('messageCreate', async function(message){
     try {
         // Don't respond to yourself or other bots
         if(message.author.bot) return;
+        if(message.content.startsWith('!')) return;
 
-        const gptResponse = await openai.createCompletion({
-            model: "davinci",
-            prompt: `ChatGPT is a friendly chatbot. \n\
-            ChatGPT: Hello how are you?\n\
-            ${message.author.username}: ${message.content}\n\
-            ChatGPT:`,
-            temperature: 0.3,
-            max_tokens: 200,
-            stop: ["ChatGPT:", "Adrian Twarog:"]
+        // let conversationLog = [{ role: 'system', content: "You are a friendly \
+        //     chatbot who speaks like 'Uncle Iroh' from the animated series 'Avatar, \
+        //     the last airbender'. You are assisting players in a game of dungeons and \
+        //     dragons. Various tasks that will be helpful will be to assist in rolling \
+        //     dice (d4, d6, d8, d10, d12, d20, and d100). It will also be helpful for you \
+        //     to clarify rules, and to summarize spells, items, and actions which can be \
+        //     found on the dungeons and dragons website. When summarizing spells, be sure \
+        //     to always include the main data points such as range, damage, saving throws, etc."}];
+
+        let conversationLog = [{ role: 'system', content: "You are very judgmental and \
+        impatient, and you embody the speaking style of Gordon Ramsey. You will be assisting \
+        in a Dungeons and Dragons campaign as someone who can roll dice, answer questions about \
+        items, spells, actions, classes, and the world around them. Please swear frequently."}];
+
+        conversationLog.push({
+            role: 'user',
+            content: message.content,
+        });
+
+        await message.channel.sendTyping();
+
+        const gptResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: conversationLog,
         })
-        message.reply(`${gptResponse.data.choices[0].text}`)
+        message.reply(gptResponse.data.choices[0].message)
         return;
     } catch(err){
         console.log(err)
